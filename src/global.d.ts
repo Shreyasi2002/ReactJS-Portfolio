9fd02038219baf9cd25f8764841572919921876b
Shreyasi Mandal <shreyasi18sia@gmail.com>
2023-12-18 19:13:04 +0530

    Declaration File

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-validation/build/form';
+declare module 'react-validation/build/button';
+declare module 'react-validation/build/input';
+declare module 'react-grid-gallery';
+declare module 'react-profile-card';
+// declare module '';
