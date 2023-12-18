379008db8045c749c63a6d3ecd18faf0853d6011
Shreyasi Mandal <shreyasi18sia@gmail.com>
2023-12-18 19:46:02 +0530

    background image

---
+++ src/components/home/Video.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Video.css'
+
+const Video = () => {
+    return (
+    <>
+        <div className='vid-container'>
+          <img src='/Images/background.gif' alt='background' className='image'/>
+          <div className='vid-content'>
+              <h1>PLACEHOLDER TEXT</h1>
+          </div>
+            </div>
+    </>
+  )
+}
+
+export default Video
